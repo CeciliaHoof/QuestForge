a6eb00b9b15a26ff6c0b1c1cbfb1adbd5645f1da
Cecilia <ccchimento@gmail.com>
2024-02-12 13:11:29 -0700

    initial commit

---
+++ server/migrations/versions/3389a3844a64_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 3389a3844a64
+Revises: 
+Create Date: 2024-02-12 13:11:29.351467
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3389a3844a64'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
