4e6ce5692135c8b9c4ea9755dc45a73d0190421f
Cecilia <ccchimento@gmail.com>
2024-02-13 15:38:18 -0700

    adventurer functionality

--- server/app.py
+++ server/app.py
@@ -2,14 +2,8 @@ from config import app, migrate
 from db_utils import *
 from models import db
 
-#Tasks:
-# Create Adventurer ✅
-# Delete Adventurer (remove from db) ✅
-#
-
 def display_welcome():
   print("Welcome to QuestForge!")
-  print("Ready to create your first adventurer and start questing?")
 
 def display_main_menu():
   print("Main Menu")
@@ -18,7 +12,7 @@ def display_main_menu():
   print("3. Exit QuestForge")
 
 def get_main_choice():
-  return input("What is your choice (1/2/3)?")
+  return input("What is your choice (1/2/3)? ")
 
 def display_all_quests():
   print("Welcome to the Quest board! From here you can view all quests or view quests based on difficulty")
@@ -27,38 +21,66 @@ def display_all_quests():
     print(f'{quest.title} \n {quest.description} \n Difficulty: {quest.difficulty} \n')
 
 def display_all_adventurers():
-  print("Welcome to the Tavern, where all of our adventures wait to be hired to a quest. From here you can hire an adventurer to assign them quests, or you can delete an delete an adventurer. \n Here are all of the adventurers:")
   adventurers = get_all_adventurers()
   if len(adventurers) == 0:
-    print("Create some adventurers!")
+    print("There are currently no adventures waiting for a quest! Create some adventurers! \n")
   else:
     for adventurer in adventurers:
-      print(f'{adventurer.name} \n Class: {adventurer.adventurer_class} \n Level: {adventurer.level} \n')
-  adventurer_menu()
-  adventurer_choice = get_adventurer_choice()
-  if adventurer_choice == "1":
-    hire_adventurer()
-  elif adventurer_choice == "2":
-    create_adventurer()
-  elif adventurer_choice == "3":
-    delete_adventurer()
+      print(f'{adventurer.id} | {adventurer.name} \n')
+
+def enter_tavern():
+  print("Welcome to the Tavern, where all of our adventures wait to be hired to a quest. From here you can hire an adventurer to assign them quests, or you can delete an delete an adventurer. \n Here are all of the adventurers: \n")
+  display_all_adventurers()
+  display_adventurer_menu()
+
+def display_adventurer_menu():
+  if len(get_all_adventurers()) == 0:
+    print("Adventure Menu")
+    print("1. Create an Adventurer")
+    print("2. Return to Main Menu")
+    adventurer_choice = input("What is your choice (1/2)? ")
   else:
-    print("Invalid choice. Please inter 1, 2, or 3.")
+    print("Adventure Menu")
+    print("1. Create an Adventurer")
+    print("2. Hire an Adventurer")
+    print("3. View Adventurer Details")
+    print("4. Fire an Adventurer")
+    print("5. Return to Main Menu")
+    adventurer_choice = input("What is your choice (1/2/3/4/5)? ")
+  handle_adventurer_choice(adventurer_choice)
 
-def adventurer_menu():
-  print("Adventure Menu")
-  print("1. Hire an Adventurer (assign them to a quest)")
-  print("2. Create a new Adventurer")
-  print("3. Fire an Adventurer")
+def handle_adventurer_choice(choice):
+  if choice == "1":
+    create_adventurer()
+    enter_tavern()
+  elif choice == "2":
+    hire_adventurer()
+  elif choice == "3":
+    adventurer = input("Which adventurer do you want to see? ")
+    view_adventurer(adventurer)
+  elif choice == "4":
+    delete_adventurer()
+    enter_tavern()
+  elif choice == "5":
+    return
+  else:
+    print("Invalid choice. Please enter 1, 2, 3, 4, or 5.")
 
-def get_adventurer_choice():
-  return input("What is your choice (1/2/3)?")
+def view_adventurer(adventurer):
+  get_adventurer_details(adventurer)
+  print('1. Return to Tavern')
+  print('2. Return to Main Menu')
+  choice = input("Where would you like to go now? (1/2)? ")
+  if choice == '1':
+    enter_tavern()
+  elif choice == '2':
+    return
+  else:
+    print("Invalid choice. Please enter 1, 2, 3, 4, or 5.")
 
 def hire_adventurer():
-  print("Here are your adventurers:")
-  adventurers = get_all_adventurers()
-  for adventure in adventurers:
-    print(f'{adventure.id}: {adventure.name} \n')
+  print("Here are your adventurers:\n")
+  display_all_adventurers()
   adventurer = input("Which Adventure would you like to hire (1/2/3...)?")
   print("Here are the quests that still need an adventurer:")
   quests = get_unassigned_quests()
@@ -66,24 +88,37 @@ def hire_adventurer():
     print(f'{quest.id}: {quest.title} \n Difficulty: {quest.difficulty} \n')
   quest_hire = input("Which quest is this adventurer going to tackle? (1/2/3...)")
   assign_adventurer_to_quest(adventurer, quest_hire)
+  display_hire_menu()
+  
 
+def display_hire_menu():
+  print("Hire Menu")
+  print("1. Keep Hiring Adventurers")
+  print("2. Return to Tavern")
+  hire_choice = input("What is your choice (1/2)? ")
+  handle_hire_choice(hire_choice)
+
+def handle_hire_choice(choice):
+  if choice == "1":
+    hire_adventurer()
+  elif choice == "2":
+    enter_tavern()
+  else:
+    print("Invalid input, please enter 1 or 2.")
+  
 if __name__ == "__main__":
   with app.app_context():
     migrate.init_app(app, db)
     display_welcome()
-    create_adventurer()
     while True:
       display_main_menu()
       choice = get_main_choice()
       if choice == "1":
         display_all_quests()
       elif choice == "2":
-        display_all_adventurers()
+        enter_tavern()
       elif choice == '3':
         print("Thanks for playing QuestForge")
         break
       else:
         print("Invalid choice. Please inter 1, 2, or 3.")
-    #create_adventurer()
-
-    # remove pass and write your cli logic
--- server/db_utils.py
+++ server/db_utils.py
@@ -6,6 +6,9 @@ def get_all_quests():
 def get_all_adventurers():
     return db.session.query(Adventurer).all()
 
+def get_unassigned_quests():
+    return db.session.query(Quest).filter(Quest.adventurer_id.is_(None))
+
 def create_adventurer():
     #Get input from the user:
     name = input("Enter the name of the adventurer: ")
@@ -16,23 +19,20 @@ def create_adventurer():
     db.session.add(new_adventurer)
     db.session.commit()
 
-    print(f"Adventurer {name} created successfully!")
+    print(f"Adventurer {name} created successfully! \n")
 
 def delete_adventurer():
     #Get input from user:
-    name = input("Enter the name of the adventurer to fire: ").lower()
-    adventurer = db.session.query(Adventurer).filter(db.func.lower(Adventurer.name) == name).first()
+    id = input("Which adventurer would you like to fire (1/2/3...)? ")
+    adventurer = db.session.query(Adventurer).filter(Adventurer.id == id).first()
 
     #If adventurer is in database, delete it
     if adventurer:
         db.session.delete(adventurer)
         db.session.commit()
-        print(f"Adventurer {name} deleted successfully!")
+        print(f"Adventurer {adventurer.name} deleted successfully!")
     else:
-        print(f"No adventurer found with the name {name}.")
-
-def get_unassigned_quests():
-    return db.session.query(Quest).filter(Quest.adventurer_id.is_(None))
+        print(f"That adventurer doesn't exist. Please check your input.")
 
 def assign_adventurer_to_quest(adventurer_select, quest_select):
 
@@ -40,8 +40,24 @@ def assign_adventurer_to_quest(adventurer_select, quest_select):
     quest = db.session.query(Quest).filter(Quest.id == quest_select).first()
 
     if adventurer and quest:
-        quest.adventurer = adventurer
+        quest.adventurer_id = adventurer.id
         db.session.commit()
-        print(f"Adventurer {adventurer.name} assigned to quest {quest.title} successfully!")
+        print(f"Adventurer {adventurer.name} assigned to quest {quest.title} successfully! \n")
     else:
         print("Invalid adventurer or quest. Please check your input.")
+
+def get_adventurer_details(id):
+    adventurer = db.session.query(Adventurer).filter(Adventurer.id == id).first()
+    
+    if adventurer:
+        quests = adventurer.quests
+        quest_display = ''
+        if len(quests) == 0:
+            quest_display = f"{adventurer.name} has not been hired for any quests"
+        for quest in quests:
+            quest_display += f'<{quest.title}>'
+        print(f"\n{adventurer.name} \n Class: {adventurer.adventurer_class} \n Level: {adventurer.level} \n Experience: {adventurer.experience} \n Quests: {quest_display}")
+    else:
+        print("Invalid adventurer. Please check your input.")
+
+    print('\n')
